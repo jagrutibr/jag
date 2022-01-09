60bebe673ca829201fa431924493daaab752ad4a
jagruti <jagrutiborse9@gmail.com>
2022-01-09 12:25:09 +0530

    Added java demo application files

---
+++ src/main/java/Java_jenkins_in_docker/App.java
@@ -0,0 +1,14 @@
+package Java_jenkins_in_docker;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+       
+            System.out.println(checkIfInputIsAnEvenNumber(122)); // Testing in the main method
+        }
+    
+        public static boolean checkIfInputIsAnEvenNumber(int number){
+            return number % 2 == 0;
+        }
+    }
